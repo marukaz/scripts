4f567f3749292a6f4ed71598ab68b94793bd44fa
marukaz <marukaz.jh@gmail.com>
2018-12-05 14:14:49 +0900

    fix fairseq_gen2pair.py

--- fairseq_gen2pair.py
+++ fairseq_gen2pair.py
@@ -12,6 +12,6 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', required=True)
+    parser.add_argument('filename')
     args = parser.parse_args()
     main(args)
