5dd2e800123038707237765ec6d3f0c3b69b61fd
marukaz <marukaz.jh@gmail.com>
2018-12-05 14:20:34 +0900

    yen to \

--- fairseq_gen2pair.py
+++ fairseq_gen2pair.py
@@ -5,9 +5,9 @@ def main(args):
     with open(args.filename) as rf, open('reference.txt', 'w') as wfr, open('generation.txt', 'w') as wfg:
         for line in rf:
             if line.startswith('T'):
-                wfr.write(line.split('¥t')[-1])
+                wfr.write(line.split('\t')[-1])
             elif line.startswith('H'):
-                wfg.write(line.split('¥t')[-1])
+                wfg.write(line.split('\t')[-1])
 
 
 if __name__ == "__main__":
