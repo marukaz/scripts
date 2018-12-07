71a574e0c5f1457aa9df9fc4e3ea813a3282683f
Kazuki <marukaz.jh@gmail.com>
2018-12-07 14:16:29 +0900

    new scripts and fix calc_fairseq_len.py

--- del_ref_longest.py
+++ del_ref_longest.py
@@ -4,19 +4,16 @@ import numpy as np
 
 
 def main(args):
-    count = 0
-    ref_shortest = 0
     with open(args.filename) as f:
         for line in f:
             try:
                 d = json.loads(line)
             except json.JSONDecodeError:
                 continue
-            shortest = np.argmax([len(hypo['text']) for hypo in d['hypos']])
-            if shortest == 0:
-                ref_shortest += 1
-            count += 1
-    print(ref_shortest/count)
+            longest = np.argmax([len(hypo['text']) for hypo in d['hypos']])
+            if longest != 0:
+                print(line, end='')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
