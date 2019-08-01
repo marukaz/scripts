ca77d94ca00b9f221e21c1ebf62218ec115604c9
marukaz <marukaz.jh@gmail.com>
2019-08-02 03:16:55 +0900

    add concat token option

--- calculate_rouge.py
+++ calculate_rouge.py
@@ -11,13 +11,25 @@ import numpy as np
 from sumeval.metrics.rouge import RougeCalculator
 
 
-def read_file(filename):
-    return [line.strip() if not '\t' in line else [subline.strip() for subline in line.strip().split('\t')] for line in open(filename)]
+def concat_tokens(tokens):
+    return ''.join(tokens.split(' '))
+
+
+def read_file(filename, do_concat):
+    with open(filename) as f:
+        if do_concat:
+             lines= [concat_tokens(line.strip()) for line in f]
+        else:
+            lines = [line.strip() for line in f]
+    if '\t' in lines[0]:
+        lines = [[subline.strip() for subline in line.split('\t')]
+                for line in lines]
+    return lines
 
 
 def main(args):
-    system_out = read_file(args.system_output)
-    reference_list = read_file(args.reference)
+    system_out = read_file(args.system_out, args.concat_tokens)
+    reference_list = read_file(args.reference, args.concat_tokens)
     rouge4one = RougeCalculator(stopwords=True, lang="ja")
     rouge4other = RougeCalculator(stopwords=False, lang="ja")
     rougeone_list = []
@@ -38,5 +50,6 @@ if __name__ == "__main__":
                         required=True, help='specify the system output file name')
     parser.add_argument('-r', '--reference', dest='reference',
                         required=True, help='specify the reference file name')
+    parser.add_argument('-c', '--concat-tokens', action='store true')
     args = parser.parse_args()
     main(args)
