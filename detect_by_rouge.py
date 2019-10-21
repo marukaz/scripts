b7344862a87b91a353cf8396018da133e354b60e
marukaz <marukaz.jh@gmail.com>
2019-10-21 17:53:43 +0900

    count percent

--- detect_by_rouge.py
+++ detect_by_rouge.py
@@ -3,6 +3,13 @@ import argparse
 from sumeval.metrics.rouge import RougeCalculator
 
 
+def concat(snt):
+    snt = snt.replace(' ', '')
+    if snt.startswith('▁'):
+        snt= snt[1:]
+    return snt
+
+
 def main(args):
     if args.rouge == '1':
         RC = RougeCalculator(stopwords=True, lang=args.lang)
@@ -14,16 +21,23 @@ def main(args):
         else:
             rouge = RC.rouge_l
 
+    line_num = 0
+    detect_num = 0
     with open(args.path1) as f1, open(args.path1src) as f1s, open(args.path2) as f2, open(args.path2src) as f2s:
         for l1, l1s, l2, l2s in zip(f1, f1s, f2, f2s):
+            line_num += 1
             if args.concat_tokens:
-                l1s = l1s.replace(' ', '')
-                l1 = l1.replace(' ', '')
-                l2s = l2s.replace(' ', '')
-                l2 = l2.replace(' ', '')
+                l1s = concat(l1s)
+                l1 = concat(l1)
+                l2s = concat(l2s)
+                l2 = concat(l2)
             score = rouge(summary=l1, references=l2)
             if score < args.threshold:
+                detect_num += 1
                 print(l1s, '=====> '+l1, l2s, '=====> '+l2)
+    print('lines: ', line_num)
+    print('detect: ', detect_num)
+    print(f'detected percent: {f:.2%}')
 
 
 if __name__ == "__main__":
