f1b6812a5c656aa1f3db9b2926c69e6823fc75c0
Kazuki Matsumaru <marukaz.jh@gmail.com>
2019-11-17 19:42:19 +0900

    Update

--- fairseq_preprocess.sh
+++ fairseq_preprocess.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-TRAIN=train;
-VALID=dev;
-TEST=test;
+TRAIN=train
+VALID=dev
+TEST=test
 
 usage_exit() {
         echo "Usage: $0 [-d DATA] [-v VALID] item ..." 1>&2
         exit 1
 }
 
-while getopts d:t:v:e:b:h OPT
+while getopts d:t:v:e:h OPT
 do
     case $OPT in
         d)  DATA=$OPTARG
@@ -20,8 +20,6 @@ do
             ;;
         e)  TEST=$OPTARG
             ;;
-        b)  BASE=$OPTARG
-            ;;
         h)  usage_exit
             ;;
         \?) usage_exit
@@ -31,8 +29,8 @@ done
 
 shift $((OPTIND - 1))
 
-source $BASE/venvs/fairseq/bin/activate;
+export PATH="$PATH:/home/matsumaru/.local/bin" 
 
 fairseq-preprocess --source-lang src --target-lang tgt \
 --trainpref $DATA/$TRAIN --validpref $DATA/$VALID --testpref $DATA/$TEST \
---destdir ${DATA}_bin --workers 32
+--destdir ${DATA}_bin --workers 8
--- tokenize_file.py
+++ tokenize_file.py
@@ -14,6 +14,8 @@ def main(args):
     wp = BertTokenizer.from_pretrained(args.tokenizer_data, do_lower_case=False)
     with path.open() as rf, save_path.open('w') as wf:
         for line in tqdm(rf):
+            if args.replace_sharp:
+                line = line.replace('#', '1')
             tokenized = wp._tokenize(line)
             print(' '.join(tokenized), file=wf)
 
@@ -21,5 +23,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('path')
     parser.add_argument('-d', '--tokenizer-data')
+    parser.add_argument('-r', '--replace-sharp', action='store_true')
     args = parser.parse_args()
     main(args)
