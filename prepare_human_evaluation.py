2a0b37a08500d49493aad90a5be5ba787cc42282
acb11164rn <acb11164rn@es4.abci.local>
2019-12-05 01:07:13 +0900

    Update

---
+++ prepare_human_evaluation.py
@@ -0,0 +1,34 @@
+import argparse
+import random
+from pathlib import Path
+
+
+def main(args):
+    random.seed(args.seed)
+    save_dir = Path(args.output_dir)
+    save_dir.mkdir(exist_ok=True)
+    save_flag_path = save_dir/'shuffle_flag.txt'
+    save_tsv_path = save_dir/'eval.tsv'
+    with open(args.src) as sf, open(args.ours) as of, open(args.base) as bf,\
+        save_flag_path.open('w') as ff, save_tsv_path.open('w') as tf:
+        for src, ours, base in zip(sf, of, bf):
+            src = src.strip()
+            ours = ours.strip()
+            base = base.strip()
+            if random.random() >= 0.5:
+                print('True', file=ff)
+                print('\t'.join((src, base, ours)), file=tf)
+            else:
+                print('False', file=ff)
+                print('\t'.join((src, ours, base)), file=tf)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src')
+    parser.add_argument('--ours')
+    parser.add_argument('--base')
+    parser.add_argument('--output-dir')
+    parser.add_argument('--seed', type=int, default=516)
+    args = parser.parse_args()
+    main(args)
---
+++ sample_fairseq_output.py
@@ -0,0 +1,46 @@
+import argparse
+import random
+from pathlib import Path
+
+from utils import process_bpe_symbol
+
+
+def extract(line, remove_bpe):
+    sp = line.split('\t')
+    id_ = int(sp[0][2:])
+    snt = sp[-1]
+    snt = process_bpe_symbol(snt, remove_bpe)
+    return id_, snt
+
+def main(args):
+    random.seed(args.seed)
+    path = Path(args.filepath)
+    save_hypo_path = path.parent/f'{path.name}.hypo.sample{args.sample_num}'
+    save_src_path = path.parent/f'{path.name}.src.sample{args.sample_num}'
+    save_ids_path = path.parent/f'{path.name}.ids.sample{args.sample_num}'
+    sample_ids = random.sample(range(args.example_num), k=args.sample_num)
+    with save_ids_path.open('w') as f:
+        for id_ in sample_ids:
+            print(id_, file=f)
+
+    with open(args.filepath) as f, save_hypo_path.open('w') as hf, save_src_path.open('w') as sf:
+        for line in f:
+            if line.startswith('S'):
+                id_, snt = extract(line.strip(), args.remove_bpe)
+                if id_ in sample_ids:
+                    print(snt, file=sf)
+            elif line.startswith('H'):
+                id_, snt = extract(line.strip(), args.remove_bpe)
+                if id_ in sample_ids:
+                    print(snt, file=hf)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath')
+    parser.add_argument('--seed', type=int, default=516)
+    parser.add_argument('--example-num', type=int, default=10000)
+    parser.add_argument('--sample-num', type=int, default=100)
+    parser.add_argument('--remove-bpe', default=None)
+    args = parser.parse_args()
+    main(args)
