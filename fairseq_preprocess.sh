b7980549d36ebf2d3b3c275badd31e9e751b4a98
Kazuki Matsumaru <marukaz.jh@gmail.com>
2019-12-16 22:04:36 +0900

    Update

--- fairseq_preprocess.sh
+++ fairseq_preprocess.sh
@@ -8,27 +8,32 @@ TEST=test
 WORKER=8
 
 usage_exit() {
-        echo "Usage: [-d data path to preprocess] [-s extension for source lang, defalut: $SRC] [-t extension for target lang, default: $TGT] [-r train prefix, defalut: $TRAIN] [-v valid prefix, default: $VALID] [-e test prefix, default: $TEST]" 1>&2
+        echo "Usage: [-p path to raw text dir] [-d destination dir]\
+         [-s extension for source lang, defalut: $SRC] [-t extension for target lang, default: $TGT]\
+         [-r train prefix, defalut: $TRAIN] [-v valid prefix, default: $VALID] [-e test prefix, default: $TEST]\
+         [-w num of workers default: $WORKER]" 1>&2
         exit 1
 }
 
-while getopts d:s:t:r:v:e:w:h OPT
+while getopts p:d:s:t:r:v:e:w:h OPT
 do
     case $OPT in
-        d)  DATA=$OPTARG
+        p)  PATH_DIR=$OPTARG
             ;;
-	s)  SRC=$OPTARG
-	    ;;
-	t)  TGT=$OPTARG
-	    ;;
+        d)  DEST_DIR=$OPTARG
+            ;;
+        s)  SRC=$OPTARG
+            ;;
+        t)  TGT=$OPTARG
+            ;;
         r)  TRAIN=$OPTARG
             ;;
         v)  VALID=$OPTARG
             ;;
         e)  TEST=$OPTARG
             ;;
-	w)  WORKER=$OPTARG
-	    ;;
+        w)  WORKER=$OPTARG
+            ;;
         h)  usage_exit
             ;;
         \?) usage_exit
@@ -38,6 +43,10 @@ done
 
 shift $((OPTIND - 1))
 
+if [ -z "$DEST_DIR" ]; then
+　　DEST_DIR=${PATH_DIR}_bin
+fi
+
 fairseq-preprocess --source-lang $SRC --target-lang $TGT \
---trainpref $DATA/$TRAIN --validpref $DATA/$VALID --testpref $DATA/$TEST \
---destdir ${DATA}_bin --workers $WORKER
+--trainpref $PATH_DIR/$TRAIN --validpref $PATH_DIR/$VALID --testpref $PATH_DIR/$TEST \
+--destdir $DEST_DIR --workers $WORKER
